7801dcd56d986e30e90789ea1f9b5c923994dcfd
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-04-24 13:56:57 +0300

    skills kısmı da tamamlandı.

---
+++ Core_Proje/BusinessLayer/Concrete/SkillManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SkillManager : ISkillService
+    {
+        ISkillDal _skillDall;
+
+        public SkillManager(ISkillDal skillDall)
+        {
+            _skillDall = skillDall;
+        }
+
+        public void TAdd(Skill t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Skill t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Skill TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Skill> TGetList()
+        {
+            return _skillDall.GetList();
+        }
+
+        public void TUpdate(Skill t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
