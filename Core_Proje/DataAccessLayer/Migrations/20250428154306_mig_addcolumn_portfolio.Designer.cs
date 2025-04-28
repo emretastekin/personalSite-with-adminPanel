6e5edb5c2a22c12b03f59c74d17c2efac092078c
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-04-28 20:00:48 +0300

    portfolio ve experience kısımları view component olarak kullanılıp, dinamik olarak ayarlandı.

--- Core_Proje/DataAccessLayer/Migrations/20250428154306_mig_addcolumn_portfolio.Designer.cs
+++ Core_Proje/DataAccessLayer/Migrations/20250428154306_mig_addcolumn_portfolio.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250428154306_mig_addcolumn_portfolio")]
+    partial class mig_addcolumn_portfolio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -187,10 +189,6 @@ namespace DataAccessLayer.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("PortfolioID"), 1L, 1);
-
-                    b.Property<string>("ImageUrl")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("ImageUrl2")
                         .IsRequired()
---
+++ Core_Proje/DataAccessLayer/Migrations/20250428154306_mig_addcolumn_portfolio.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_addcolumn_portfolio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Portfolios",
+                newName: "ProjectUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl2",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl2",
+                table: "Portfolios");
+
+            migrationBuilder.RenameColumn(
+                name: "ProjectUrl",
+                table: "Portfolios",
+                newName: "ImageUrl");
+        }
+    }
+}
