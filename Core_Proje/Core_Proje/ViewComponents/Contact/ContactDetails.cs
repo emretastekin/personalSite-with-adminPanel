72e40c69eec983ca2b0b78081923aaffd3a8904b
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-05-06 20:46:58 +0300

    contact bölümünün bir kısmı yapıldı.

---
+++ Core_Proje/Core_Proje/ViewComponents/Contact/ContactDetails.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Contact
+{
+    public class ContactDetails : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
