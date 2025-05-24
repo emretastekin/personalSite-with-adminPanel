ba9c41c31584ef9bdbd741a0c2bf5a2efd944372
Emre Tastekin <emretastekin2369@gmail.com>
2025-05-24 13:52:34 +0300

    admin kısmında yetenekler kısmı veri tabanından çekilip tablo halinde gösterildi.

--- Core_Proje/Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Core_Proje/Controllers/SkillController.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+
         public IActionResult Index()
         {
-            return View();
+            var values = skillManager.TGetList();
+
+            return View(values);
         }
     }
 }
